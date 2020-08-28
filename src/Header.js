6e04cb6cfb6a112aafdd85eb71ffe4370a3ab5ad
t-sakurai816 <poppun_0816@icloud.com>
2020-08-28 11:24:07 +0900

    add material-icons

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
   return (
-    <div>
+    <div className="header">
+      <PersonIcon />
       <h2>I am a header</h2>
+      <ForumIcon />
     </div>
   )
 }
