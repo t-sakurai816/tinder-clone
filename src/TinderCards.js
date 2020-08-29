3c89bec59e548fef35eaf94d958526302b1c130c
t-sakurai816 <poppun_0816@icloud.com>
2020-08-29 10:44:42 +0900

    deconnect firebase

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, /*useEffect*/ } from 'react';
 import TinderCard from 'react-tinder-card';
-import database from './firebase'
+// import database from './firebase'
 import './TinderCards.css'
 
 const TinderCards = () => {
-  const [people, setPeople] = useState([]);
+  const [people, /*setPeople*/] = useState([
+    {
+      name:'肉バルダイニング MEAT DE MEET ニュー新橋ビル店',
+      url : 'https://imgfp.hotp.jp/IMGH/59/54/P031835954/P031835954_238.jpg'
+    },
+    {
+      name: '日本酒と個室居酒屋 まぐろ奉行とかに代官 新橋店',
+      url : 'https://imgfp.hotp.jp/IMGH/70/71/P035287071/P035287071_238.jpg'
+    },
+    {
+      name: '個室居酒屋 峰八 みねはち 新橋本店',
+      url: 'https://imgfp.hotp.jp/IMGH/67/27/P036246727/P036246727_238.jpg'
+    }
+  ]);
   
   const onSwipe = (direction) => { //スワイプの方向
     console.log('You swiped: ' + direction)
@@ -21,20 +34,20 @@ const TinderCards = () => {
   }
 
   //Piece of code which runs based on a condition
-  useEffect(() => {
-    //this is where the code runs...
+  // useEffect(() => {
+  //   //this is where the code runs...
 
-    const unsubscribe = database
-      .collection('people')
-      .onSnapshot((snapshot) =>
-        setPeople(snapshot.docs.map(doc => doc.data()))
-      );
+  //   const unsubscribe = database
+  //     .collection('people')
+  //     .onSnapshot((snapshot) =>
+  //       setPeople(snapshot.docs.map(doc => doc.data()))
+  //     );
 
-    return () => {
-      //this is cleanup...
-      unsubscribe();
-    };
-  }, []);
+  //   return () => {
+  //     //this is cleanup...
+  //     // unsubscribe();
+  //   };
+  // }, []);
 
   // BAD
   // const people = [];
